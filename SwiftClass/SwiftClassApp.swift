0d56aa3a25c3a32f6c507d7bcf762230dc19e7f4
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-24 17:43:44 +0000

    Initial Commit

---
+++ SwiftClass/SwiftClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftClassApp.swift
+//  SwiftClass
+//
+//  Created by Tony Chaidinis on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
