f0b7fe0ad3defaa7dd428238310596e4beb7901a
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-26 16:46:49 +0000

    cc

--- SwiftClass/Shapes/Shape.swift
+++ SwiftClass/Shapes/Shape.swift
@@ -2,15 +2,21 @@
 import Foundation
 
 protocol Shape {
-  var area: Int { get }
-  var perimeter: Int { get }
+  var area: Int { get set }
+  var perimeter: Int { get set}
 }
 
 struct Rectangle: Shape {
+  var perimeter: Int
   let height: Int
   let width: Int
   
-  func area() {
+  var area: {
+    
+  }
+  
+  func area() -> Int {
     return height * width
   }
 }
+
